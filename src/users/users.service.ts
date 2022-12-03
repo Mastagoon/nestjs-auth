5caf33285837a36cb6f2dac46dc16d0ab5b27d58
Mastagoon <inzilagiway@gmail.com>
2022-12-03 14:18:16 +0300

    thing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+  async create(input: { email: string; password: string; name: string }) {
+    const user = await this.prisma.user.create({
+      data: {
+        email: input.email,
+        password: input.password,
+      },
+    });
+    return user;
+  }
+  async findOne(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    return user;
+  }
+}
